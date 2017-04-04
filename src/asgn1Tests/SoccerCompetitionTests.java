0bd2fc59c1c63c83e7637ae798102cc486682b56
DESKTOP-LHAPPQM\Schwa <pschwartz914@gmail.com>
2017-04-04 14:47:57 +1000

    More test

--- src/asgn1Tests/SoccerCompetitionTests.java
+++ src/asgn1Tests/SoccerCompetitionTests.java
@@ -20,7 +20,21 @@ import asgn1SoccerCompetition.SoccerTeam;
  */
 public class SoccerCompetitionTests {
 	SoccerCompetition sc;
+	SoccerTeam team1;
+	SoccerTeam team2;
+	SoccerTeam team3;
+	SoccerTeam team4;
 	
+	public void registerTeams() throws TeamException, LeagueException, CompetitionException {
+		team1 = new SoccerTeam("Chelsea", "Blues");
+		team2 = new SoccerTeam("Manchester City", "City");
+		team3 = new SoccerTeam("Manchester United", "Red Devils");
+		team4 = new SoccerTeam("Southampton", "Saints");
+		sc.getLeague(0).registerTeam(team1);
+		sc.getLeague(0).registerTeam(team2);
+		sc.getLeague(0).registerTeam(team3);
+		sc.getLeague(0).registerTeam(team4);
+	}
 	@Before
 	public void setup() {
 		sc = new SoccerCompetition("Test", 2, 4);
@@ -33,13 +47,12 @@ public class SoccerCompetitionTests {
 		assertEquals(expectedLeague, sc.getLeague(0));
 	}
 	
-	//Why is this and next one not working.
-	@Test(expected = LeagueException.class)
+	@Test
 	public void startSeasonWithNoTeams() throws LeagueException, CompetitionException{
-		sc.getLeague(0).startNewSeason();
-		//sc.startSeason();
-		//Boolean offSeason = sc.getLeague(0).isOffSeason();
-		//assertTrue(offSeason);
+		//sc.getLeague(0).startNewSeason();
+		sc.startSeason();
+		Boolean offSeason = sc.getLeague(0).isOffSeason();
+		assertTrue(offSeason);
 	}
 	
 	@Test(expected = LeagueException.class)
@@ -64,17 +77,20 @@ public class SoccerCompetitionTests {
 	
 	@Test
 	public void startSeason() throws LeagueException, CompetitionException, TeamException{
-		SoccerTeam team1 = new SoccerTeam("Chelsea", "Blues");
-		SoccerTeam team2 = new SoccerTeam("Manchester City", "City");
-		SoccerTeam team3 = new SoccerTeam("Manchester United", "Red Devils");
-		SoccerTeam team4 = new SoccerTeam("Southampton", "Saints");
-		sc.getLeague(0).registerTeam(team1);
-		sc.getLeague(0).registerTeam(team2);
-		sc.getLeague(0).registerTeam(team3);
-		sc.getLeague(0).registerTeam(team4);
+		registerTeams();
 		sc.startSeason();
 		Boolean offSeason = sc.getLeague(0).isOffSeason();
 		assertFalse(offSeason);
+	}
+	
+	@Test
+	public void endSeasonWork() throws TeamException, LeagueException, CompetitionException {
+		registerTeams();
+		sc.startSeason();
+		sc.endSeason();
+		assertEquals(team1, sc.getLeague(0).getTopTeam());
+		
+		
 	}
 
 }
